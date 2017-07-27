c8c05cd2b803c39da0f870e171ea444f9fe4c515
Lindsey Rix <lindsey.rix@blackbaud.com>
2017-07-27 16:46:18 -0400

    creating a dashboard

---
+++ src/app/grid-dnc-feedback.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'grid-dnc-feedback',
+  templateUrl: './grid-dnc-feedback.component.html',
+  styleUrls: ['./grid-feedback.component.scss']
+})
+export class GridDNCeedbackComponent { }
